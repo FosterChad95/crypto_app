56fba5b632001d38bb7211e587214619e861dad2
Chad Foster <fosterchad95@gmail.com>
2021-11-03 22:57:59 -0400

    Rendered Data on screen and added components to homepage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,20 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
   "x-rapidapi-key": "4b2654a8d6mshed8c92443989f0ep1cdfd1jsn4513aebf01f5",
 };
 
-const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest("/exchanges"),
+      query: () => createRequest("/coins"),
     }),
   }),
 });
+
+export const { useGetCryptosQuery } = cryptoApi;
